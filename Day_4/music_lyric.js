b83865c67040b6537081b3b9458ff1694688b829
DetachmLiu <59466661+DetachmLiu@users.noreply.github.com>
2020-06-26 22:22:11 +0800

    Day_4.5 —— 完成歌词的实时显示
    成功修复歌词的显示和解析bug！

--- Day_4/music_lyric.js
+++ Day_4/music_lyric.js
@@ -1,6 +1,6 @@
 const currentSong = {
     songName : "",
-    songId : "412785957",
+    songId : "413812448",
     singers : "",
     lyricUrl : "https://api.itooi.cn/netease/lrc?id=",
     picUrl : "https://api.itooi.cn/netease/pic?id="
@@ -12,6 +12,9 @@ function getLyric() {
         type: "get",
         dataType: "text",
         success: parseLyric,
+        error: function () {
+            alert("Laji");
+        }
     })
 }
 
@@ -22,12 +25,11 @@ function parseLyric(lyrics) {
     let lyricData = lyrics.split("[");
 
     // 遍历 lyricData，分割时间戳和歌词信息
-    // 歌词首部加个空白行用来显示
-    lyricInfo.push({
-        time : 0,
-        lyric : ""
-    });
     // 跳过下标 0 的空字符串
+    lyricInfo.push({
+        time : -1,
+        lyric : "&nbsp"
+    })
     for (let i = 1; i < lyricData.length; i++) {
         const line = lyricData[i].split("]");
         // 如果 "[]" 内的信息不是时间，那就跳过这一句
@@ -36,24 +38,24 @@ function parseLyric(lyrics) {
         }
         const m = line[0].substr(0, 2);
         const s = line[0].substr(3, 2);
-        const times = m * 60 + s;
+        const ms = line[0].substr(6, 2);
+        const times = parseInt(m) * 60 + parseInt(s) + parseInt(ms) * 0.01;
         // 遇到了后一句歌词时间在前一句歌词之前，
         // 这种情况会出现在外语歌中，翻译在前半段，
         // 原文在后半段，此时清空 lyricInfo，
         // 用于记录原文
-        if (lyricInfo.pop().time > times) {
+        if (lyricInfo[lyricInfo.length - 1].time > times) {
             lyricInfo = [];
             lyricInfo.push({
-                time : 0,
-                lyric : ""
-            });
+                time : -1,
+                lyric : "&nbsp"
+            })
         }
         lyricInfo.push({
             time : times,
-            lyric : line[1]
+            lyric : line[1].trim()
         });
         if (i === lyricData.length - 1) {
-            // 歌词尾部增加空白行便于显示
             lyricInfo.push({
                 time : times + 1,
                 lyric : ""
@@ -63,15 +65,40 @@ function parseLyric(lyrics) {
 }
 
 // 插入到主页的 html 中去
-$("#player").addEventListener("timeupdate", function () {
-    const index = lyricInfo.findIndex(item => {
-        return item.time >= this.currentTime;
-    })
-    const html = `
-        <li>${lyricInfo[index - 1].lyric}</li>
-        <li>${lyricInfo[index].lyric}</li>
-        <li>${lyricInfo[index + 1].lyric}</li>
-    `;
+// function addLyric() {
+//     const ul = $("<ul></ul>");
+//     for (let i = 0; i < lyricInfo.length; i++) {
+//         ul.append("<li>" + lyricInfo[i].lyric + "</li>");
+//     }
+//     $("#lyrics").append(ul);
+// }
 
-    return $(".lyric").innerHTML = html;
-})
+window.onload = function() {
+    document.getElementById("player").addEventListener("timeupdate", function () {
+        const index = lyricInfo.findIndex(item => {
+            return item.time >= this.currentTime;
+        });
+        if (index < 0) {
+            return;
+        }
+
+        if (index < 2) {
+            document.getElementById("lyrics").innerHTML = `
+                <li style="font-size: 20px; color: #01AAED;">
+                        ${lyricInfo[index - 1].lyric || "&nbsp"}
+                </li>
+                <li>${lyricInfo[index].lyric}</li>
+                <li>${lyricInfo[index + 1].lyric}</li>
+            `;
+            return;
+        }
+
+        document.getElementById("lyrics").innerHTML = `
+            <li>${lyricInfo[index - 2].lyric}</li>
+            <li style="font-size: 20px; color: #01AAED;">
+                    ${lyricInfo[index - 1].lyric}
+            </li>
+            <li>${lyricInfo[index].lyric}</li>
+        `;
+    })
+}
