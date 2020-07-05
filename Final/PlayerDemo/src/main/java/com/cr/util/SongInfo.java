5e08e64b86cc88b0044a09a75178c7dfbb380b6e
DetachmLiu <59466661+DetachmLiu@users.noreply.github.com>
2020-07-05 16:05:39 +0800

    Final —— 验收阶段
    上传验收阶段最后的Demo。

---
+++ Final/PlayerDemo/src/main/java/com/cr/util/SongInfo.java
@@ -0,0 +1,13 @@
+package com.cr.util;
+
+public class SongInfo {
+    private String name;
+    private String singers;
+    private String songId;
+
+    public SongInfo(String name, String singers, String songId) {
+        this.name = name;
+        this.singers = singers;
+        this.songId = songId;
+    }
+}
